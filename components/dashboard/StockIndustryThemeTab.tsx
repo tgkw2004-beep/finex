c23fd6bb02d810b7babd840b0f0201c8b17c1705
utlim <kendor@naver.com>
2026-03-09 12:49:03 +0900

    업종/테마 탭: 열지도 + 테마 둘 다 세로 배치 (탭 없음)

--- components/dashboard/StockIndustryThemeTab.tsx
+++ components/dashboard/StockIndustryThemeTab.tsx
@@ -1,11 +1,17 @@
 "use client"
 
 import StockIndustryTab from './StockIndustryTab'
+import StockThemeTab from './StockThemeTab'
 
 interface StockIndustryThemeTabProps {
     symbol: string
 }
 
 export default function StockIndustryThemeTab({ symbol }: StockIndustryThemeTabProps) {
-    return <StockIndustryTab symbol={symbol} />
+    return (
+        <div className="space-y-6">
+            <StockIndustryTab symbol={symbol} />
+            <StockThemeTab symbol={symbol} />
+        </div>
+    )
 }
